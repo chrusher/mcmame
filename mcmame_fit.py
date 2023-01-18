6965b0971c4f160b79389a87401760fcf5412b18
Chris Usher <chris.usher@astro.su.se>
2023-01-18 19:11:04 +0100

    stricter priors

--- mcmame_fit.py
+++ mcmame_fit.py
@@ -98,9 +98,9 @@ if __name__ == '__main__':
     parser.add_argument('--output', help='output_name')
     parser.add_argument('-N', type=int, help='Number of parallel processes')
     parser.add_argument('--age-lower', type=float, default=0.001, help='Lower limit of age prior')
-    parser.add_argument('--age-upper', type=float, default=15.84, help='Upper limit of age prior')
+    parser.add_argument('--age-upper', type=float, default=15.0, help='Upper limit of age prior')
     parser.add_argument('--metal-lower', type=float, default=-3., help='Lower limit of [Z/H] prior')
-    parser.add_argument('--metal-upper', type=float, default=0.7, help='Upper limit of [Z/H] prior')
+    parser.add_argument('--metal-upper', type=float, default=0.5, help='Upper limit of [Z/H] prior')
     parser.add_argument('--A_V-lower', type=float, default=0., help='Lower limit of A_V prior')
     parser.add_argument('--A_V-upper', type=float, default=np.inf, help='Upper limit of A_V prior')      
     
